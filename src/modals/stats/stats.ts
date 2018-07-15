c5551fbd36cb6ecf5ed47e68dd481000212382e5
tkiddleYoobic <35728583+tkiddleYoobic@users.noreply.github.com>
2018-07-15 14:39:12 +0100

    feat(stats): Add stats selector

--- src/modals/stats/stats.ts
+++ src/modals/stats/stats.ts
@@ -24,6 +24,12 @@ export class StatsPage {
   labels: any = {week: [], month: [], year: []};
   values: any = {week: [], month: [], year: []};
   month = ['01','02','03','04','05','06','07','08','09','10','11','12'];
+  title: string = '';
+  isBackward:boolean = false;
+  isForward:boolean = false;
+  totalWeeks: any = {total: 0, index: 1};
+  totalMonths: any = {total: 0, index: 1};
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, private aFdatabase: AngularFireDatabase, private afAuth: AngularFireAuth) {
   this.time = 'week';
@@ -35,36 +41,41 @@ export class StatsPage {
       data.forEach((object) => {
        let values = [];
        let labels = [];
+       let week: string = null;
        let obj: any = object;
+       this.totalWeeks.total = this.totalWeeks.total + 1;
        obj.forEach(element => {
-           values.push(element.Value);
-           labels.push(element.Day);
+            values.push(element.Value);
+            labels.push(element.Day);
+            week = element.Title;
        });
-       this.labels.week.push(labels);
+       this.labels.week.push({week: week, labels: labels});
        this.values.week.push(values);
       });
     });
 
     this.month.forEach((month) => {
         let dbM = this.aFdatabase.list(this.afAuth.auth.currentUser.uid + '/CBTValues/Month/' + month);
-        if (dbM) {
-            let values = []
-            let labels = []
-            dbM.valueChanges().subscribe((data) => {
-                data.forEach((object) => {
+        let values = []
+        let labels = []
+
+        dbM.valueChanges().subscribe((data) => {
+        if (data.length > 0) {
+            this.totalMonths.total = this.totalMonths.total + 1;            
+            data.forEach((object) => {
                 let obj: any = object;
                 
                 labels.push(obj.Day);
                 var value =+ obj.Value;
                 values.push(value);
                 
-                });
-                if (labels.length > 0 ){
-                    this.labels.month.push({month: month, labels: labels});
-                    this.values.month.push({month: month, values: values});                
-                } 
             });
         }
+            if (labels.length > 0 ){
+                this.labels.month.push({month: month, labels: labels});
+                this.values.month.push({month: month, values: values});                
+            } 
+        });
     });
     
     let db = this.aFdatabase.list(this.afAuth.auth.currentUser.uid + '/CBTValues/Year');
@@ -78,7 +89,10 @@ export class StatsPage {
     });
 
    setTimeout(() => {
-       this.setGraph(this.labels.week[0], this.values.week[0]);
+       console.log( 'weeks', this.totalWeeks);
+       console.log( 'months', this.totalMonths);
+       this.setGraph(this.labels.week[this.totalWeeks.total - this.totalWeeks.index].labels, this.values.week[this.totalWeeks.total - this.totalWeeks.index], this.labels.week[this.totalWeeks.total - this.totalWeeks.index].week);
+       this.checkSelectorButtonDisable(this.totalWeeks.index, this.totalWeeks.total);      
    }, 1000);
    
   }
@@ -87,59 +101,80 @@ export class StatsPage {
       console.log(this.labels);
       if (this.time === 'all') {
         this.setGraph(this.labels.year, this.values.year);
-      } else if (this.time === 'month') {
-        this.setGraph(this.labels.month[0].labels, this.values.month[0].values);
-      } else {
-        this.setGraph(this.labels.week[0], this.values.week[0]);
-      }
+        this.checkSelectorButtonDisable(0, 0);      
+    } else if (this.time === 'month') {
+        this.setGraph(this.labels.month[this.totalMonths.total - this.totalMonths.index].labels, this.values.month[this.totalMonths.total - this.totalMonths.index].values, this.labels.month[this.totalMonths.total - this.totalMonths.index].month);
+        this.checkSelectorButtonDisable(this.totalMonths.index, this.totalMonths.total);      
+    } else {
+       this.setGraph(this.labels.week[this.totalWeeks.total - this.totalWeeks.index].labels, this.values.week[this.totalWeeks.total - this.totalWeeks.index], this.labels.week[this.totalWeeks.total - this.totalWeeks.index].week);
+       this.checkSelectorButtonDisable(this.totalWeeks.index, this.totalWeeks.total);      
+    }
 
     }
   
-    setGraph(labels: any, values: any) {
+    setGraph(labels: any, values: any, title: string = '') {
         console.log('labels', labels);
         console.log('values', values);
+        console.log('title', title);
+        this.title = title;
             this.chart = new Chart(this.barCanvas.nativeElement, {
-                type: 'bar',
-                      data: {
-                          labels: labels,
-                          datasets: [{
-                              
-                              data: values,
-                              backgroundColor: '#02b5ef'
-                          }]
-                      },
-                      options: {
-                          scales: {
-                              yAxes: [{
-                                  ticks: {
-                                      beginAtZero:true,
-                                      max: 10,
-                                      stepSize: 1,
-                                  },
-                                  gridLines: {
-                                      color: 'rgba(0,0,0,0)'
-                                  }
-                              }],
-                              xAxes: [{
-                                barThickness: 10,
-                                gridLines: {
-                                    color: 'rgba(0,0,0,0)'
-                                }
-                              }]
-                          },
-                          title: {
-                            display:false
+            type: 'bar',
+            data: {
+                labels: labels,
+                datasets: [{
+                    
+                    data: values,
+                    backgroundColor: '#02b5ef'
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true,
+                            max: 10,
+                            stepSize: 1,
                         },
-                        legend: {
-                            display:false
+                        gridLines: {
+                            color: 'rgba(0,0,0,0)'
                         }
-                      }
-           
-                  });
+                    }],
+                    xAxes: [{
+                        barThickness: 10,
+                        gridLines: {
+                            color: 'rgba(0,0,0,0)'
+                        }
+                    }]
+                },
+                title: {
+                    display: false,
+                },
+                legend: {
+                    display:false
+                }
+            }
+        });
     }
 
   close() {
     this.viewCtrl.dismiss();
+  }
+
+  onArrowPress(change: number) {
+    if (this.time === 'month') {
+        this.totalMonths.index = this.totalMonths.index + change;
+    } else if (this.time === 'week') {
+        console.log('change', change);
+        this.totalWeeks.index = this.totalWeeks.index + change;
+        console.log('index', this.totalWeeks.index);
+    }
+    this.segmentChanged();
+  }
+
+
+  checkSelectorButtonDisable(index: number, total: number) {
+      this.isForward = index > 1;
+      this.isBackward = index !== total;
   }
 
 }
